9805548b84c14b827069b1ad00b39895470be9cb
Vitaly <vitaly1090@mail.ru>
2025-02-06 13:14:33 +1000

    add sout

--- src/main/java/by/javaguru/Main.java
+++ src/main/java/by/javaguru/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello world!!!!");
         System.out.println("It's terribly difficult");
         System.out.println("I'm happy");
+        System.out.println("!!!!");
     }
 }
