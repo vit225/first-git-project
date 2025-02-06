76216a920fb2b122c36a1ca1a95217e0c0aa91db
Vitaly <vitaly1090@mail.ru>
2025-02-06 13:12:11 +1000

    change sout

--- src/main/java/by/javaguru/Main.java
+++ src/main/java/by/javaguru/Main.java
@@ -2,6 +2,7 @@ package by.javaguru;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("GIT DEVELOP");
         System.out.println("Hello world!");
         System.out.println("Hello world!!!!");
         System.out.println("It's terribly difficult");
