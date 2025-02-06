5a29ac28c1376a4b1691ddb308aac5ae6a37616d
Vitaly <vitaly1090@mail.ru>
2025-02-06 12:41:40 +1000

    Change class Main add sout

--- src/main/java/by/javaguru/Main.java
+++ src/main/java/by/javaguru/Main.java
@@ -3,5 +3,6 @@ package by.javaguru;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!!!");
     }
 }
